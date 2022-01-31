c32be78da25e2d27bcceb382a7d5d921f373582c
deepa <deepa@DESKTOP-AJQTNNR>
2022-01-31 17:50:08 +0530

    IOCProj31-Anno-TraditionalDependencyLookup

---
+++ IOCProj31-Anno-TraditionalDependencyLookup/src/main/java/com/nit/beans/Cricketer.java
@@ -0,0 +1,43 @@
+package com.nit.beans;
+
+import org.springframework.context.annotation.Lazy;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.stereotype.Component;
+
+@Component("cktr")
+@Lazy(value = true)
+public class Cricketer {
+	
+	//No HAS-A property of dependent class
+	
+	public Cricketer() {
+		System.out.println("Cricketer :: 0-param constructor");
+	}
+	
+	public void bowling() {
+		System.out.println("Cricketer is bowling");
+	}
+	
+	public void batting() {
+		System.out.println("Cricketer is batting");
+		
+		//Create additional IOC container
+		ClassPathXmlApplicationContext ctx = new ClassPathXmlApplicationContext("com/nit/cfgs/applicationContext.xml");
+		
+		//Perform dependency lookup method to get dependent class object
+		Bat bat = ctx.getBean("bat",Bat.class);  //DL code
+		int score = bat.scoreRuns();
+		System.out.println("Cricketer batting score is : "+score);
+		
+		//close stream
+		ctx.close();
+	}
+	
+	public void fielding() {
+		System.out.println("Cricketer is fielding");
+	}
+	
+	public void wicketKeeping() {
+		System.out.println("Cricketer is wicketKeeping");
+	}
+}//class
